478ab6b7ecacb0a58ff0ab4a86cfb5952f437921
demagalawrence <demagalawrence8@gmail.com>
2026-04-01 22:47:36 +0300

    Update views.py

--- movie_management_system/movieflex/views.py
+++ movie_management_system/movieflex/views.py
@@ -19,7 +19,7 @@ from mongoengine.errors import DoesNotExist
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-# ---------------- Home section ---------
+# ---------------- Home section --------
 
 def home(request):
     movies = list(Movie.objects())
